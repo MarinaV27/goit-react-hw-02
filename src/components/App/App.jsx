02e130f5b11a5736cbad627aa4b65f7b96ecff77
MarinaV27 <Vasilishinamari@gmail.com>
2024-05-09 22:56:59 +0300

    add  Description

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,8 @@
+import Description from "../Description/Description.jsx"
 import css from "./App.module.css"
 
 export default function App() {
-    return 
+    return (
+        <Description />
+    )
 }

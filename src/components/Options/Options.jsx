3ded1c99d630935cd3d9151900aad7658a78a218
MarinaV27 <Vasilishinamari@gmail.com>
2024-05-21 20:40:12 +0300

    add button Reset

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,13 +1,15 @@
 import css from "./Options.module.css"
 
-export default function Options({ updateFeedback, totalFeedback }){
+export default function Options({ updateFeedback, totalFeedback, resetFeedback }){
    
     return (
         <div className={css.buttons}>
             <button className={css.btn} onClick={() => updateFeedback(`good`)}>Good</button>
             <button className={css.btn} onClick={() => updateFeedback(`neutral`)}>Neutral</button>
             <button className={css.btn} onClick={() => updateFeedback(`bad`)}>Bad</button>
-            
+            {totalFeedback > 0 && (
+            <button className={css.btn} onClick={resetFeedback}>Reset</button>
+            )}
         </div>
 
     )
